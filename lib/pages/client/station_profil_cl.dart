8572b18ceb713624a50832c42a7d1a905817dec3
Iliyass ZAMOURI <iliyass.zamouri@gmail.com>
2021-01-31 23:11:40 +0100

    menu - proflie (client)
    sidemenu / profile dial lclient

--- lib/pages/client/station_profil_cl.dart
+++ lib/pages/client/station_profil_cl.dart
@@ -1,8 +1,6 @@
 import 'package:feul_delivery/animations/FadeAnimation.dart';
 import 'package:flutter/material.dart';
 
-
-
 class StationProfilCl extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -19,40 +17,54 @@ class StationProfilCl extends StatelessWidget {
                   collapseMode: CollapseMode.pin,
                   background: Container(
                     decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage('assets/emma.jpg'),
-                        fit: BoxFit.cover
-                      )
-                    ),
+                        image: DecorationImage(
+                            image: AssetImage('assets/emma.jpg'),
+                            fit: BoxFit.cover)),
                     child: Container(
                       decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          begin: Alignment.bottomRight,
-                          colors: [
+                          gradient: LinearGradient(
+                              begin: Alignment.bottomRight,
+                              colors: [
                             Colors.black,
                             Colors.black.withOpacity(.3)
-                          ]
-                        )
-                      ),
+                          ])),
                       child: Padding(
                         padding: EdgeInsets.all(20),
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           mainAxisAlignment: MainAxisAlignment.end,
                           children: <Widget>[
-                            FadeAnimation(1, Text("Total", style: 
-                              TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 40)
-                            ,)),
-                            SizedBox(height: 20,),
+                            FadeAnimation(
+                                1,
+                                Text(
+                                  "Total",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 40),
+                                )),
+                            SizedBox(
+                              height: 20,
+                            ),
                             Row(
                               children: <Widget>[
-                                FadeAnimation(1.2, 
-                                  Text("60 Like", style: TextStyle(color: Colors.grey, fontSize: 16),)
+                                FadeAnimation(
+                                    1.2,
+                                    Text(
+                                      "60 Like",
+                                      style: TextStyle(
+                                          color: Colors.grey, fontSize: 16),
+                                    )),
+                                SizedBox(
+                                  width: 50,
                                 ),
-                                SizedBox(width: 50,),
-                                FadeAnimation(1.3, Text("24 deslike", style: 
-                                  TextStyle(color: Colors.grey, fontSize: 16)
-                                ,))
+                                FadeAnimation(
+                                    1.3,
+                                    Text(
+                                      "24 deslike",
+                                      style: TextStyle(
+                                          color: Colors.grey, fontSize: 16),
+                                    ))
                               ],
                             )
                           ],
@@ -69,59 +81,136 @@ class StationProfilCl extends StatelessWidget {
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        FadeAnimation(1.6, 
-                          Text("Description", style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),)
+                        FadeAnimation(
+                            1.6,
+                            Text(
+                              "Description",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold),
+                            )),
+                        FadeAnimation(
+                            1.6,
+                            Text(
+                              "Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla ",
+                              style: TextStyle(color: Colors.grey, height: 1.4),
+                            )),
+                        SizedBox(
+                          height: 40,
                         ),
-                        FadeAnimation(1.6, Text("Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla ", 
-                        style: TextStyle(color: Colors.grey, height: 1.4),)),
-                        SizedBox(height: 40,),
-                        FadeAnimation(1.6, 
-                          Text("adrisse", style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),)
+                        FadeAnimation(
+                            1.6,
+                            Text(
+                              "adrisse",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold),
+                            )),
+                        SizedBox(
+                          height: 10,
                         ),
-                        SizedBox(height: 10,),
-                        FadeAnimation(1.6, 
-                          Text("rue sefrou, Narjiss, Fés, Maroc", style: TextStyle(color: Colors.grey),)
+                        FadeAnimation(
+                            1.6,
+                            Text(
+                              "rue sefrou, Narjiss, Fés, Maroc",
+                              style: TextStyle(color: Colors.grey),
+                            )),
+                        SizedBox(
+                          height: 20,
                         ),
-                        SizedBox(height: 20,),
-                        FadeAnimation(1.6, 
-                          Text("N° de livreur", style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),)
+                        FadeAnimation(
+                            1.6,
+                            Text(
+                              "N° de livreur",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold),
+                            )),
+                        SizedBox(
+                          height: 10,
                         ),
-                        SizedBox(height: 10,),
-                        FadeAnimation(1.6, 
-                          Text("4 livreur", style: TextStyle(color: Colors.grey),)
+                        FadeAnimation(
+                            1.6,
+                            Text(
+                              "4 livreur",
+                              style: TextStyle(color: Colors.grey),
+                            )),
+                        SizedBox(
+                          height: 20,
                         ),
-                        SizedBox(height: 20,),
-                        FadeAnimation(1.6, 
-                          Text("N° de téléfone", style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),)
+                        FadeAnimation(
+                            1.6,
+                            Text(
+                              "N° de téléfone",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold),
+                            )),
+                        SizedBox(
+                          height: 10,
                         ),
-                        SizedBox(height: 10,),
-                        FadeAnimation(1.6, 
-                          Text("+212654543476", style: TextStyle(color: Colors.grey),)
-                        ),SizedBox(height: 20,),
-                        FadeAnimation(1.6, 
-                          Text("email", style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),)
+                        FadeAnimation(
+                            1.6,
+                            Text(
+                              "+212654543476",
+                              style: TextStyle(color: Colors.grey),
+                            )),
+                        SizedBox(
+                          height: 20,
                         ),
-                        SizedBox(height: 10,),
-                        FadeAnimation(1.6, 
-                          Text("man3rf@total.com", style: TextStyle(color: Colors.grey),)
+                        FadeAnimation(
+                            1.6,
+                            Text(
+                              "email",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold),
+                            )),
+                        SizedBox(
+                          height: 10,
                         ),
-                        SizedBox(height: 20,),
-                        FadeAnimation(1.6, 
-                          Text("images", style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),)
+                        FadeAnimation(
+                            1.6,
+                            Text(
+                              "man3rf@total.com",
+                              style: TextStyle(color: Colors.grey),
+                            )),
+                        SizedBox(
+                          height: 20,
                         ),
-                        SizedBox(height: 20,),
-                        FadeAnimation(1.8, Container(
-                          height: 200,
-                          child: ListView(
-                            scrollDirection: Axis.horizontal,
-                            children: <Widget>[
-                              makeVideo(image: 'assets/emma.jpg'),
-                              makeVideo(image: 'assets/emma.jpg'),
-                              makeVideo(image: 'assets/emma.jpg'),
-                            ],
-                          ),
-                        )),
-                        SizedBox(height: 120,)
+                        FadeAnimation(
+                            1.6,
+                            Text(
+                              "images",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold),
+                            )),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        FadeAnimation(
+                            1.8,
+                            Container(
+                              height: 200,
+                              child: ListView(
+                                scrollDirection: Axis.horizontal,
+                                children: <Widget>[
+                                  makeVideo(image: 'assets/emma.jpg'),
+                                  makeVideo(image: 'assets/emma.jpg'),
+                                  makeVideo(image: 'assets/emma.jpg'),
+                                ],
+                              ),
+                            )),
+                        SizedBox(
+                          height: 120,
+                        )
                       ],
                     ),
                   )
@@ -134,15 +223,19 @@ class StationProfilCl extends StatelessWidget {
             child: Container(
               child: Align(
                 alignment: Alignment.bottomCenter,
-                child: FadeAnimation(2,
+                child: FadeAnimation(
+                  2,
                   Container(
                     margin: EdgeInsets.symmetric(horizontal: 30),
                     height: 50,
                     decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50),
-                      color: Colors.orange[900]
-                    ),
-                    child: Align(child: Text("Faire un command", style: TextStyle(color: Colors.white),)),
+                        borderRadius: BorderRadius.circular(50),
+                        color: Colors.orange[900]),
+                    child: Align(
+                        child: Text(
+                      "Faire un command",
+                      style: TextStyle(color: Colors.white),
+                    )),
                   ),
                 ),
               ),
@@ -155,16 +248,13 @@ class StationProfilCl extends StatelessWidget {
 
   Widget makeVideo({image}) {
     return AspectRatio(
-      aspectRatio: 1.5/ 1,
+      aspectRatio: 1.5 / 1,
       child: Container(
         margin: EdgeInsets.only(right: 20),
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          image: DecorationImage(
-            image: AssetImage(image),
-            fit: BoxFit.cover
-          )
-        ),
+            borderRadius: BorderRadius.circular(20),
+            image:
+                DecorationImage(image: AssetImage(image), fit: BoxFit.cover)),
       ),
     );
   }
