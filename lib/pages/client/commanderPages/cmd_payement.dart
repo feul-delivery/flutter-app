d44d05c4191907706547c21e06846d9b6620609e
Iliyass ZAMOURI <iliyass.zamouri@gmail.com>
2021-02-13 20:38:02 +0100

    payment

---
+++ lib/pages/client/commanderPages/cmd_payement.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CommandePayment extends StatelessWidget {
+  const CommandePayment({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red[900],
+      ),
+    );
+  }
+}
