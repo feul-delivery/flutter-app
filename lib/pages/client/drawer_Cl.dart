ce2e229d51f76aa6e9174ebb168324ba4258875c
Iliyass ZAMOURI <iliyass.zamouri@gmail.com>
2021-02-13 17:56:07 +0100

    Nigga

--- lib/pages/client/drawer_Cl.dart
+++ lib/pages/client/drawer_Cl.dart
@@ -14,147 +14,145 @@ class DrawerCL extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Container(
-        color: Colors.white,
-        width: 260,
-        child: Column(children: [
-          Container(
-            child: Padding(
-              padding: EdgeInsets.only(top: 50.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  CircleAvatar(
-                    radius: 50.0,
-                    backgroundImage: NetworkImage(
-                      "https://images.unsplash.com/photo-1594616838951-c155f8d978a0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-                    ),
+    return Container(
+      color: Colors.white,
+      width: 260,
+      child: Column(children: [
+        Container(
+          child: Padding(
+            padding: EdgeInsets.only(top: 50.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: NetworkImage(
+                    "https://images.unsplash.com/photo-1594616838951-c155f8d978a0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
                   ),
-                  SizedBox(
-                    height: 5.0,
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                Text(
+                  "Nom Prénom",
+                  style: TextStyle(
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.w800,
                   ),
-                  Text(
-                    "Nom Prénom",
-                    style: TextStyle(
-                      fontSize: 22.0,
-                      fontWeight: FontWeight.w800,
-                    ),
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                Text(
+                  "Contact@gmail.com",
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w400,
                   ),
-                  SizedBox(
-                    height: 5.0,
-                  ),
-                  Text(
-                    "Contact@gmail.com",
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
-          SizedBox(
-            height: 20.0,
+        ),
+        SizedBox(
+          height: 20.0,
+        ),
+        ListTile(
+          onTap: () {
+            selectedIndex = 0;
+            Navigator.of(context).push(MaterialPageRoute(
+                builder: (BuildContext context) => IndexCl()));
+          },
+          leading: Icon(
+            Icons.home,
+            color: Colors.red[900],
           ),
-          ListTile(
-            onTap: () {
-              selectedIndex = 0;
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (BuildContext context) => IndexCl()));
-            },
-            leading: Icon(
-              Icons.home,
-              color: Colors.red[900],
-            ),
-            title: Text(
-              "Acceuil",
-              style: textStyle,
-            ),
+          title: Text(
+            "Acceuil",
+            style: textStyle,
           ),
-          ListTile(
-            onTap: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (BuildContext context) => profileCLModifier()));
-            },
-            leading: Icon(
-              Icons.person,
-              color: Colors.red[900],
-            ),
-            title: Text(
-              "Mon Profile",
-              style: textStyle,
-            ),
+        ),
+        ListTile(
+          onTap: () {
+            Navigator.of(context).push(MaterialPageRoute(
+                builder: (BuildContext context) => profileCLModifier()));
+          },
+          leading: Icon(
+            Icons.person,
+            color: Colors.red[900],
           ),
-          ListTile(
-            onTap: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (BuildContext context) => FavorisCl()));
-            },
-            leading: Icon(
-              Icons.favorite,
-              color: Colors.red[900],
-            ),
-            title: Text(
-              "Mes favoris",
-              style: textStyle,
-            ),
+          title: Text(
+            "Mon Profile",
+            style: textStyle,
           ),
-          ListTile(
-            onTap: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (BuildContext context) => CommandeCl()));
-            },
-            leading: Icon(
-              Icons.receipt,
-              color: Colors.red[900],
-            ),
-            title: Text(
-              "Mes commandes",
-              style: textStyle,
-            ),
+        ),
+        ListTile(
+          onTap: () {
+            Navigator.of(context).push(MaterialPageRoute(
+                builder: (BuildContext context) => FavorisCl()));
+          },
+          leading: Icon(
+            Icons.favorite,
+            color: Colors.red[900],
           ),
-          ListTile(
-            onTap: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (BuildContext context) => SettingsCl()));
-            },
-            leading: Icon(
-              Icons.settings,
-              color: Colors.red[900],
-            ),
-            title: Text(
-              "Paramètres",
-              style: textStyle,
-            ),
+          title: Text(
+            "Mes favoris",
+            style: textStyle,
           ),
-          Expanded(
-              child: Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              height: 40,
-              margin: EdgeInsets.symmetric(horizontal: 45, vertical: 20),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.red[700]),
-              child: Center(
-                child: FlatButton(
-                  onPressed: () {
-                    Navigator.of(context).pushReplacement(MaterialPageRoute(
-                        builder: (BuildContext context) => LoginPage()));
-                  },
-                  child: Text(
-                    "Se déconnecter",
-                    style: buttonStyle,
-                  ),
+        ),
+        ListTile(
+          onTap: () {
+            Navigator.of(context).push(MaterialPageRoute(
+                builder: (BuildContext context) => CommandeCl()));
+          },
+          leading: Icon(
+            Icons.receipt,
+            color: Colors.red[900],
+          ),
+          title: Text(
+            "Mes commandes",
+            style: textStyle,
+          ),
+        ),
+        ListTile(
+          onTap: () {
+            Navigator.of(context).push(MaterialPageRoute(
+                builder: (BuildContext context) => SettingsCl()));
+          },
+          leading: Icon(
+            Icons.settings,
+            color: Colors.red[900],
+          ),
+          title: Text(
+            "Paramètres",
+            style: textStyle,
+          ),
+        ),
+        Expanded(
+            child: Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: 40,
+            margin: EdgeInsets.symmetric(horizontal: 45, vertical: 20),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.red[700]),
+            child: Center(
+              child: FlatButton(
+                onPressed: () {
+                  Navigator.of(context).pushReplacement(MaterialPageRoute(
+                      builder: (BuildContext context) => LoginPage()));
+                },
+                child: Text(
+                  "Se déconnecter",
+                  style: buttonStyle,
                 ),
               ),
             ),
-          ))
-        ]),
-      ),
+          ),
+        ))
+      ]),
     );
   }
 }
