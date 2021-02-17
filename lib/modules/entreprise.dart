001ccbaa80b8a9c4feb8fd6b3683f467b405d540
Iliyass ZAMOURI <iliyass.zamouri@gmail.com>
2021-02-17 01:42:21 +0100

    fixes (Everthing is ok)

--- lib/modules/entreprise.dart
+++ lib/modules/entreprise.dart
@@ -1,15 +1,19 @@
 class Entreprise {
   final String identreprise;
-  final String volume;
+  final String titre;
+  final String description;
+  final String tele;
+  final String email;
   final String adresse;
-  final String dateheurec;
-  final String dateheurel;
-  final String matricule;
-  final String color;
-  final double prixtotal;
-  final String statut;
-  final String methode;
-  final String idclient;
-  final String idtype;
-  Entreprise({this.identreprise, this.volume, th});
+  final int like;
+  final int dislike;
+  Entreprise(
+      {this.identreprise,
+      this.titre,
+      this.description,
+      this.tele,
+      this.email,
+      this.adresse,
+      this.like,
+      this.dislike});
 }
