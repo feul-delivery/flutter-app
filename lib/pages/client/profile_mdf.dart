16178099e0d2e907eb10b2e796db4a365400f136
Iliyass ZAMOURI <iliyass.zamouri@gmail.com>
2021-02-17 14:18:02 +0100

    more things

--- lib/pages/client/profile_mdf.dart
+++ lib/pages/client/profile_mdf.dart
@@ -1,4 +1,4 @@
-import 'package:feul_delivery/shared/textStyles.dart';
+import 'package:feul_delivery/shared/text_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'bbar_cl.dart';
