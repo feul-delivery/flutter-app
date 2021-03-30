04d40153012510f1afcb62ca461d8805684f262d
imadHS <78317253+imadHS@users.noreply.github.com>
2021-03-30 16:43:30 +0100

    x

---
+++ lib/pages/station/cartCommandes.dart
@@ -0,0 +1,184 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class ToutCommandes extends StatefulWidget {
+
+  final DocumentSnapshot document;
+ToutCommandes(this.document);
+  @override
+  _ToutCommandesState createState() => _ToutCommandesState();
+}
+
+class _ToutCommandesState extends State<ToutCommandes> {
+  var pnom='N/A';
+
+@override
+void initState() { 
+  super.initState();
+  getNom();
+}
+Future getNom() async {
+  
+    await Firestore.instance
+        .collection('client')
+        .document(widget.document['uidclient'])
+        .get()
+        .then((value) async {
+      if (value.exists) {
+        var key1 = await value.data['prenom'];
+        var key2 = await value.data['nom'];
+        setState(() {
+          pnom = key1 + ' ' + key2;
+          print(pnom);
+        });
+      }
+    });
+}
+  @override
+  Widget build(BuildContext context) {
+      var document = widget.document;
+    DateTime date = DateTime.parse(document['dateheurec']);
+    return Container(
+      child: Column(
+        children: [
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+              decoration: new BoxDecoration(
+                  color: Colors.white70,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.black.withOpacity(0.4),
+                        blurRadius: 20,
+                        offset: Offset(0, 10))
+                  ]),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          height: 25,
+                          margin: EdgeInsets.symmetric(horizontal: 5),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.black,
+                          ),
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.only(left: 5.0, right: 5.0),
+                            child: Center(
+                                child: Text(
+                              DateFormat.Hm().format(date),
+                              style: TextStyle(color: Colors.white),
+                            )),
+                          ),
+                        ),
+                        Container(
+                          height: 25,
+                          margin: EdgeInsets.symmetric(horizontal: 5),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.black,
+                          ),
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.only(left: 5.0, right: 5.0),
+                            child: Center(
+                                child: Text(
+                              "${date.day}/${date.month}/${date.year}",
+                              style: TextStyle(color: Colors.white),
+                            )),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text('$pnom',
+                              style: TextStyle(
+                                  color: Colors.black, fontSize: 25))),
+                    ),
+                    Row(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Container(
+                                      child: Text('Volume: ',
+                                          style: TextStyle(
+                                              color: Colors.grey[800])),
+                                    ),
+                                    SizedBox(
+                                      width: 20,
+                                    ),
+                                    Container(
+                                      child: Text('${document['volume']}L',
+                                          style: TextStyle(
+                                              color: Colors.grey[600])),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Container(
+                                  child: Text('Type:',
+                                      style:
+                                          TextStyle(color: Colors.grey[800])),
+                                ),
+                                SizedBox(
+                                  width: 20,
+                                ),
+                                Container(
+                                  child: Text(document['idtype'],
+                                      style:
+                                          TextStyle(color: Colors.grey[600])),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                  child: Text('Address: ',
+                                      style:
+                                          TextStyle(color: Colors.grey[800])),
+                                ),
+                                Container(
+                                  child: Text(document['adresse'],
+                                      style:
+                                          TextStyle(color: Colors.grey[600])),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              )),
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/station/commandes_st.dart
+++ lib/pages/station/commandes_st.dart
@@ -1,10 +1,10 @@
+import 'package:FD_flutter/pages/station/cartCommandes.dart';
 import 'package:FD_flutter/pages/station/command_st.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:FD_flutter/pages/station/drawer_st.dart';
 import 'package:FD_flutter/pages/station/bbar_st.dart';
-import 'package:intl/intl.dart';
 
 import 'index_st.dart';
 
@@ -96,7 +96,7 @@ class _ToutCommandesStState extends State<ToutCommandesSt> {
                                             builder: (context) =>
                                                 CommandeDetailSt(document)));
                                   },
-                                  child: toutCommandes(document));
+                                  child: ToutCommandes(document));
                             })?.toList());
                     }
                   },
@@ -111,187 +111,4 @@ class _ToutCommandesStState extends State<ToutCommandesSt> {
     );
   }
 
-  Container toutCommandes(DocumentSnapshot document) {
-    DateTime date = DateTime.parse(document['dateheurec']);
-    return Container(
-      child: Column(
-        children: [
-          SizedBox(
-            height: 20,
-          ),
-          Container(
-              decoration: new BoxDecoration(
-                  color: Colors.white70,
-                  borderRadius: BorderRadius.circular(10),
-                  boxShadow: [
-                    BoxShadow(
-                        color: Colors.black.withOpacity(0.4),
-                        blurRadius: 20,
-                        offset: Offset(0, 10))
-                  ]),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Container(
-                          height: 25,
-                          margin: EdgeInsets.symmetric(horizontal: 5),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Colors.black,
-                          ),
-                          child: Padding(
-                            padding:
-                                const EdgeInsets.only(left: 5.0, right: 5.0),
-                            child: Center(
-                                child: Text(
-                              DateFormat.Hm().format(date),
-                              style: TextStyle(color: Colors.white),
-                            )),
-                          ),
-                        ),
-                        Container(
-                          height: 25,
-                          margin: EdgeInsets.symmetric(horizontal: 5),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Colors.black,
-                          ),
-                          child: Padding(
-                            padding:
-                                const EdgeInsets.only(left: 5.0, right: 5.0),
-                            child: Center(
-                                child: Text(
-                              "${date.day}/${date.month}/${date.year}",
-                              style: TextStyle(color: Colors.white),
-                            )),
-                          ),
-                        ),
-                      ],
-                    ),
-                    Container(
-                      child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text('REF : ${document['uidclient']}',
-                              style: TextStyle(
-                                  color: Colors.black, fontSize: 15))),
-                    ),
-                    Row(
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              children: [
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  children: [
-                                    Container(
-                                      child: Text('Volume: ',
-                                          style: TextStyle(
-                                              color: Colors.grey[800])),
-                                    ),
-                                    SizedBox(
-                                      width: 20,
-                                    ),
-                                    Container(
-                                      child: Text('${document['volume']}L',
-                                          style: TextStyle(
-                                              color: Colors.grey[600])),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Container(
-                                  child: Text('Type:',
-                                      style:
-                                          TextStyle(color: Colors.grey[800])),
-                                ),
-                                SizedBox(
-                                  width: 20,
-                                ),
-                                Container(
-                                  child: Text(document['idtype'],
-                                      style:
-                                          TextStyle(color: Colors.grey[600])),
-                                ),
-                              ],
-                            ),
-                            Row(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Container(
-                                  child: Text('Address: ',
-                                      style:
-                                          TextStyle(color: Colors.grey[800])),
-                                ),
-                                Container(
-                                  child: Text(document['adresse'],
-                                      style:
-                                          TextStyle(color: Colors.grey[600])),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              )),
-        ],
-      ),
-    );
-  }
-
-  List items = [
-    {
-      "nom": "Da7mad ELMAKKAWI",
-      "time": "17:47",
-      "date": "17/01/2021",
-      "etat": "waiting",
-      "volume": "10L",
-      "type": "Gas-oil",
-      "adresse": "Monfleuri 2, Lycée Technique, Fès",
-    },
-    {
-      "nom": "Da7mad ELMAKKAWI",
-      "time": "17:05",
-      "date": "17/01/2021",
-      "etat": "waiting",
-      "volume": "50L",
-      "type": "Gas-oil",
-      "adresse": "Monfleuri 2, Lycée Technique, Fès",
-    },
-    {
-      "nom": "Da7mad ELMAKKAWI",
-      "time": "17:01",
-      "date": "17/01/2021",
-      "etat": "waiting",
-      "volume": "43L",
-      "type": "Gas-oil",
-      "adresse": "Monfleuri 2, Lycée Technique, Fès",
-    },
-    {
-      "nom": "Da7mad ELMAKKAWI",
-      "time": "16:50",
-      "date": "17/01/2021",
-      "etat": "waiting",
-      "volume": "43L",
-      "type": "Gas-oil",
-      "adresse": "Monfleuri 2, Lycée Technique, Fès",
-    }
-  ];
 }
