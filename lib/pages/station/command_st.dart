5f330beaf46b0478a7b3716624502c3957373d8a
imadHS <78317253+imadHS@users.noreply.github.com>
2021-04-01 15:52:13 +0100

    x

--- lib/pages/station/command_st.dart
+++ lib/pages/station/command_st.dart
@@ -21,16 +21,16 @@ class _CommandeDetailStState extends State<CommandeDetailSt> {
     super.initState();
     if (widget.document['statut'] == 'done') {
       getLivreur();
-      getClient();
     }
+    getClient();
   }
 
   Future getLivreur() async {
     await Firestore.instance
         .collection('livreur')
         .document(widget.document['uidlivreur'])
-        .get()
-        .then((value) async {
+        ?.get()
+        ?.then((value) async {
       if (value.exists) {
         var key1 = await value.data['prenom'];
         var key2 = await value.data['nom'];
@@ -98,7 +98,10 @@ class _CommandeDetailStState extends State<CommandeDetailSt> {
           elevation: 1,
           actions: [
             IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  _clientDetailSheet(
+                      context, widget.document['uidclient'], clNom, clNum);
+                },
                 icon: Icon(
                   Icons.info_outline,
                   color: Colors.white,
@@ -113,10 +116,13 @@ class _CommandeDetailStState extends State<CommandeDetailSt> {
   Widget getBodyDone() {
     DateTime dateC = DateTime.parse(widget.document['dateheurec']);
     DateTime dateL = DateTime.parse(widget.document['dateheurel']);
+    int colorV = int.parse(widget.document['color'].substring(6, 16));
+    Color myColor = Color(colorV);
+    print(colorV);
     return ListView(
       children: <Widget>[
         SizedBox(
-          height: 30,
+          height: 10,
         ),
         Padding(
           padding: const EdgeInsets.only(left: 20, right: 20),
@@ -319,6 +325,7 @@ class _CommandeDetailStState extends State<CommandeDetailSt> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Icon(
                     Icons.color_lens,
@@ -335,22 +342,21 @@ class _CommandeDetailStState extends State<CommandeDetailSt> {
                         fontWeight: FontWeight.w500,
                         color: Colors.black54),
                   ),
+                  SizedBox(
+                    width: 60,
+                  ),
+                  Container(
+                    width: 60,
+                    height: 20,
+                    color: myColor,
+                  ),
                 ],
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Center(
-                child: Text(
-                  widget.document['color'],
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-                ),
               ),
             ],
           ),
         ),
         SizedBox(
-          height: 20,
+          height: 27,
         ),
         Padding(
           padding: const EdgeInsets.only(left: 20, right: 20),
@@ -436,10 +442,13 @@ class _CommandeDetailStState extends State<CommandeDetailSt> {
 
   Widget getBodyAtt() {
     DateTime dateC = DateTime.parse(widget.document['dateheurec']);
+    int colorV = int.parse(widget.document['color'].substring(6, 16));
+    Color myColor = Color(colorV);
+    print(colorV);
     return ListView(
       children: <Widget>[
         SizedBox(
-          height: 30,
+          height: 10,
         ),
         Padding(
           padding: const EdgeInsets.only(left: 20, right: 20),
@@ -564,6 +573,7 @@ class _CommandeDetailStState extends State<CommandeDetailSt> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Icon(
                     Icons.color_lens,
@@ -580,16 +590,15 @@ class _CommandeDetailStState extends State<CommandeDetailSt> {
                         fontWeight: FontWeight.w500,
                         color: Colors.black54),
                   ),
+                  SizedBox(
+                    width: 60,
+                  ),
+                  Container(
+                    width: 60,
+                    height: 17,
+                    color: myColor,
+                  ),
                 ],
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Center(
-                child: Text(
-                  widget.document['color'],
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-                ),
               ),
             ],
           ),
@@ -692,25 +701,24 @@ Future<void> _clientDetailSheet(
                 padding: const EdgeInsets.all(8.0),
                 child: Row(
                   children: [
-                    
                     Container(
-                       decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        height: MediaQuery.of(context).size.width / 5,
-                        width: MediaQuery.of(context).size.width / 5,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      height: MediaQuery.of(context).size.width / 5,
+                      width: MediaQuery.of(context).size.width / 5,
                       child: StreamBuilder<DocumentSnapshot>(
                           stream: Firestore.instance
                               .collection('client')
                               .document(id)
-                              .get()
-                              .asStream(),
+                              ?.get()
+                              ?.asStream(),
                           builder: (context, snapshot) {
                             return Column(
                               mainAxisAlignment: MainAxisAlignment.start,
                               crossAxisAlignment: CrossAxisAlignment.center,
                               children: [
-                                snapshot.data['photoURL'] == null
+                                snapshot?.data['photoURL'] == null
                                     ? CircleAvatar(
                                         radius: 50.0,
                                         backgroundImage:
@@ -733,7 +741,6 @@ Future<void> _clientDetailSheet(
                                                 radius: 30.0,
                                                 child: Icon(Icons.error)),
                                       ),
-                                
                               ],
                             );
                           }),
