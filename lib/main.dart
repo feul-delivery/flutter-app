11087b54c0d9c05443b1ce74865af0286d9c40b0
Iliyass ZAMOURI <iliyass.zamouri@gmail.com>
2021-05-03 02:16:07 +0000

    v

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,9 @@
-import 'package:FD_flutter/pages/client/index_cl.dart';
-import 'package:FD_flutter/pages/livreur/index_lv.dart';
-import 'package:FD_flutter/pages/station/index_st.dart';
 import 'package:FD_flutter/services/auth.dart';
 import 'package:FD_flutter/shared/splash.dart';
 import 'package:FD_flutter/wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
-import 'authentification/authenticate.dart';
-import 'authentification/type_compte.dart';
 import 'modules/user.dart';
 
 void main() => runApp(FuelDeliveryApp());
@@ -25,11 +20,11 @@ class FuelDeliveryApp extends StatelessWidget {
         routes: {
           '/': (context) => SplashScreen(),
           '/wrapper': (context) => Wrapper(),
-          '/wrapper/authenticate': (context) => Authenticate(),
-          '/wrapper/cl': (context) => IndexCl(),
-          '/wrapper/lv': (context) => IndexLv(),
-          '/wrapper/st': (context) => IndexSt(),
-          '/wrapper/typecpt': (context) => TypeCompte(),
+          // '/wrapper/authenticate': (context) => Authenticate(),
+          // '/wrapper/cl': (context) => IndexCl(),
+          // '/wrapper/lv': (context) => IndexLv(),
+          // '/wrapper/st': (context) => IndexSt(),
+          // '/wrapper/typecpt': (context) => TypeCompte(),
         },
       ),
     );
